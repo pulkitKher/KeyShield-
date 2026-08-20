c19ea9084d94097ff640c63e7f7737fb40cc8459
Pulkit Kher <pulkitkher867@gmail.com>
2026-08-20 16:26:11 +0530

    Changed Script

--- collector/script.js
+++ collector/script.js
@@ -1,5 +1,37 @@
 const passwordInput = document.getElementById("password");
+const eventLog = document.getElementById("eventLog");
 
-passwordInput.addEventListener("keydown",function(event){
-    console.log("Key Pressed", event.key);
+// Store all keystrokes
+let keystrokes = [];
+
+// Temporary storage for current key press
+let currentKeyDownTime = null;
+
+// Key Down Event
+passwordInput.addEventListener("keydown", function(event) {
+
+    currentKeyDownTime = performance.now();
+
+});
+
+// Key Up Event
+passwordInput.addEventListener("keyup", function(event) {
+
+    const key = event.key;
+
+    const upTime = performance.now();
+
+    const holdTime = upTime - currentKeyDownTime;
+
+    const record = {
+        key: key,
+        downTime: currentKeyDownTime,
+        upTime: upTime,
+        holdTime: holdTime
+    };
+
+    keystrokes.push(record);
+
+    console.log(keystrokes);
+
 });
